f5b3a37cd793bb46f4a2e8a3eaa468131e60c7ea
Alisher2105 <alisher.rajabov.2020@gmail.com>
2023-08-02 15:38:44 +0500

    Data Rest faqat kichik dasturlar uchun ishlatiladi.

--- src/main/java/com/example/datarest/repository/AddressRepository.java
+++ src/main/java/com/example/datarest/repository/AddressRepository.java
@@ -2,6 +2,9 @@ package com.example.datarest.repository;
 
 import com.example.datarest.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "address")
 
 public interface AddressRepository extends JpaRepository<Address, Integer> {
 }
